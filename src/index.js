ddc28450652f95042ed9d47df25d8076661753f8
Glitch (veil-plough) <none>
2019-01-09 06:54:34 +0000

    🏙🚌 Another try on the foundation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import MemoryGame from './MemoryGame';
+
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<MemoryGame />, document.getElementById('root'));
