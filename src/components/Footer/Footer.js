00d7e446a46a55976e6e7837dc1f200f49a0d1e6
Glitch (veil-plough) <none>
2019-01-27 20:22:56 +0000

    Making things worse

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// set the footer to a copyright string
+const Footer = props => (
+  <div className="footer">
+    <p>Test</p>
+  </div>
+);
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import MemoryGame from './MemoryGame';
+import Footer from "./components/Footer";
 
 
 ReactDOM.render(
